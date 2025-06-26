e85049fc9a26672839ff7b16860fa14ddb6471ac
unknown <pichkhaianikoloz@gmail.com>
2025-06-27 03:34:54 +0400

    updated settings to include static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+# static files
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
