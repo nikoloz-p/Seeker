f211423e1ae4cf6505c19b10d4c67d89d2bcd5fe
unknown <pichkhaianikoloz@gmail.com>
2025-06-27 04:27:48 +0400

    changed db name in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../db/jobs.db',
+        'NAME': BASE_DIR / '../db/main.db',
     }
 }
 
